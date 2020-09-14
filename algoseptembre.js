ab4c7d412825651117f8492366e7c7a928f273d6
stef427 <stef_427@hotmail.com>
2020-09-14 10:41:04 +0200

    exo impot

--- algoseptembre.js
+++ algoseptembre.js
@@ -1,1 +1,38 @@
-let
+// // declaration de variables
+// let prenom = prompt("Veuillez entrer votre prénom");
+// console.log(prenom);
+
+// // conditions
+// let age = Number(prompt("veuillez entrer votre âge"));
+// console.log(age);
+// if (age >= 18) {
+//     console.log("tu as " + age + " ans, tu es  majeur ");
+// } else {
+//     console.log("tu as " + age + " ans, tu es mineur ");
+// }
+
+// exo impot
+// let sexe = prompt("veuillez entrez votre genre");
+// let age = prompt("quel est votre âge");
+
+// if ((age >= 20) && (sexe == "homme")) {
+//     console.log("tu paies tes impots")
+
+// } else if (sexe == "femme" && age >= 18 && age <= 35) {
+//     console.log("tu paies tes impots")
+// } else {
+//     console.log("tu paies pas d'impot")
+// }
+
+// let sexe = prompt("veuillez entrez votre genre");
+// let age = prompt("quel est votre âge");
+
+// version courte
+// let sexe = prompt("veuillez entrez votre genre");
+// let age = prompt("quel est votre âge");
+// if ((age >= 20 && sexe == "homme") || ((age >= 18 && age <= 35) && sexe == "femme")) {
+//     console.log("tu paies tes impots")
+
+// } else {
+//     console.log("tu paies pas d'impot")
+// }
